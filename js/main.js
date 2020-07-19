424271f7d3c70e5212f03b6f16579f16dcbc8689
riabovol_dmitriy <ryabovoldima@gmail.com>
2020-07-19 09:25:13 +0300

    code header and nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-
+   menu();
+   search();
 });
+
+function menu() {
+   $(".nav__mob").on('click', function () {
+      if ($(".nav").hasClass('mob-menu')) {
+         $(".nav").removeClass('mob-menu');
+      } 
+      else {
+         $(".nav").addClass('mob-menu');
+      }
+});
+
+$(".wrapper").on('click', function(event) {
+   if (!$(event.target).closest(".nav").length) {
+      if ($(".nav").hasClass("mob-menu")) {
+         $(".nav").removeClass("mob-menu");
+      }
+   }
+});
+}
+
+function search() {
+   $(".search__mob").on('click', function () {
+         if ($(".header").hasClass('mob-search')) {
+            $(".header").removeClass('mob-search');
+         } 
+         else {
+            $(".header").addClass('mob-search');
+         }
+   });
+
+   $(".wrapper").on('click', function(event) {
+      if (!$(event.target).closest(".search").length) {
+         if ($(".header").hasClass("mob-search")) {
+            $(".header").removeClass("mob-search");
+         }
+      }
+   });
+}
